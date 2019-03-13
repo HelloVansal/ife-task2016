8f3ad9f791a1977b69aa8ad4ad98c94261847809
HelloVansal <959661369@qq.com>
2019-03-13 21:36:12 +0800

    task16 finished

--- part2/task16/task.js
+++ part2/task16/task.js
@@ -14,30 +14,13 @@ let aqiData = {};
 function addAqiData() {
     let city = document.getElementById('aqi-city-input').value;
     let qual = document.getElementById('aqi-value-input').value;
-    let num = [0,1,2,3,4,5,6,7,8,9];
-    if(!city){
-        alert('城市名称不能为空');
+    if(!/^[\u4e00-\u9fa5a-zA-Z]+$/.test(city)){
+        alert('城市名称只接受中文或英文字符');
         return false;
     }
-    else {
-        for(let i in city) {
-            if (Number(city[i]) in num) {
-                alert('城市名称只接受中文或英文字符');
-                return false;
-            }
-        }
-    }
-    if(!qual){
-        alert('空气质量指数不能为空');
+    if(!/^\d+$/.test(qual)){
+        alert('空气质量只接受整数');
         return false;
-    }
-    else{
-        for(let i in qual){
-            if(!(Number(qual[i]) in num)){
-                alert('空气质量只接受整数');
-                return false;
-            }
-        }
     }
     aqiData[city] = qual;
 }
@@ -69,27 +52,26 @@ function addBtnHandle() {
  * 点击各个删除按钮的时候的处理逻辑
  * 获取哪个城市数据被删，删除数据，更新表格显示
  */
-function delBtnHandle(i, btn) {
+function delBtnHandle(e) {
     // do sth.
-    btn[i].parentElement.parentElement.remove();
+    let city = e.target.parentNode.parentNode.childNodes[0].textContent;
+    delete aqiData[city];
     renderAqiList();
 }
 
 function init() {
-
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
     let addBtn = document.getElementById('add-btn');
     addBtn.onclick = function () {
         addBtnHandle();
-        let btn = document.getElementById('aqi-table').getElementsByTagName('input');
-        for(let i in btn){
-            btn[i].onclick = function () {
-                delBtnHandle(i, btn);
-            }
-        }
     };
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
+    let delBtn = document.getElementById('aqi-table');
+    delBtn.onclick = function (e) {
+            if(e.target.nodeName.toLowerCase() === 'input'){
+                delBtnHandle(e);
+            }
+    }
 }
 
 init();
